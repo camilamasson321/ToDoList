4cb6acf03f7c2969a18026f0d66d66a384d9c329
camilamasson321 <camilamasson321@gmail.com>
2022-06-29 21:56:38 +0000

    second

--- src/js/component/ToDo.jsx
+++ src/js/component/ToDo.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-//include images into your bundle
+
 
 //create your first component
 const Home = () => {
--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 
-//include images into your bundle
 
 //create your first component
 const Home = () => {
 	return (
-		
+	
 	);
 };
 
