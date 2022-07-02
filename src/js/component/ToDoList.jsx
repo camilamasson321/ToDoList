705e995499c130a26d17fb3247d93b393d27e553
camilamasson321 <camilamasson321@gmail.com>
2022-07-02 00:55:59 +0000

    just have to add some more styling elements to the list.

--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -1,59 +1,64 @@
-import React, {useState} from "react";
-import {AiOutlineClose} from 'react-icons/ai'
+import React, { useState } from "react";
+import { AiOutlineClose } from "react-icons/ai";
 
 //include images into your bundle
 
 //create your first component
 const Home = () => {
-	const [todo, setTodo] = useState([''])
-	const[input, setInput]=useState('')
+  const [todo, setTodo] = useState([]);
+  const [input, setInput] = useState("");
 
-	//add todos
-	const handleSubmit = (e) =>{
-		e.preventDefault()
-		const addTodo ={
-			id: Math.floor(Math.random() * 1000),
-			text: input, 
-			completed: false
-		}
-		setTodo([...todo, addTodo])
-		setInput("")
-	}
+  //add todos
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const addTodo = {
+      id: Math.floor(Math.random() * 1000),
+      text: input,
+      completed: false,
+    };
+    setTodo([...todo, addTodo]);
+    setInput("");
+  };
 
-// delete todos
-const deleteTodo =(id) =>{
-	let filteredTodos=[...todo].filter((todo) => todo.id !==id)
-	setTodo (filteredTodos)
-	console.log ('task deleted')
-}
+  // delete todos
+  const deleteTodo = (id) => {
+    let filteredTodos = [...todo].filter((todo) => todo.id !== id);
+    setTodo(filteredTodos);
+    console.log("task deleted");
+  };
 
-	return (
-		<div className="app">
-			<div className="container">
-				<h1>ToDos</h1>
+  return (
+    <div className="app">
+      <div className="container">
+        <h1>ToDos</h1>
 
-				<form onSubmit={handleSubmit}>
-					<input 
-					value={input}
-					onChange={e=> setInput(e.target.value)}
-					placeholder='No task, add a task' 
-					type=""/>
-				</form>
-				<div>
-					{todo.map(todo => (
-						<div key={todo.id}>
-							<p>{todo.text}<AiOutlineClose onClick={ () => deleteTodo(todo.id)} className='icon' />
-							</p>
-							
-						</div>
-
-					))}
-				</div>
-			</div>
-
-		</div>
-		
-	);
+        <form onSubmit={handleSubmit}>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="No task, add a task"
+            onFocus={(e) => (e.target.placeholder = "")}
+            type=""
+          />
+        </form>
+        <ul className="todotext">
+          {todo.map((task, index) => {
+            return (
+              <li className="theList" key={index}>
+                <p className="taskwords">
+                  {task.text} </p>
+                  <p className="deletebutton"><AiOutlineClose
+                    onClick={() => deleteTodo(task.id)}
+                    className="icon"
+                  />
+                </p>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
